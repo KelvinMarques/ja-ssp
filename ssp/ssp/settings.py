f5afc1126a826fbd72ecdb4b7eb40b165b9273aa
Kelvin Marques <46509359+KelvinMarques@users.noreply.github.com>
2021-10-01 10:39:19 -0300

    Update settings.py

--- ssp/ssp/settings.py
+++ ssp/ssp/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
-    'demoapp',
     'social_scrapper.apps.SocialScrapperConfig',
 ]
 
